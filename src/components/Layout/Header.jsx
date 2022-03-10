a615f7908df40bb9f9099e4b06f8986cab628855
aki0609-free <akiyamafree0609@gmail.com>
2022-03-10 17:06:46 +0900

    finish

---
+++ src/components/Layout/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import classes from './Header.module.css';
+import mealsImage from '../../assets/meals.jpg'
+import { HeaderCartButton } from './HeaderCartButton';
+
+export const Header = (props) => {
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>React Meals</h1>
+        <HeaderCartButton onClick={props.onShowCart} />
+      </header>
+      <div className={classes['main-image']}>
+        <img src={mealsImage} alt="A table of delicious food" />
+      </div>
+    </>
+  )
+}
